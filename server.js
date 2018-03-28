6a121b522be5c3802cc36f724d621b08de276e5e
Marcelo Pacobahyba <marcelo.pacobahyba@brilliantmachine.com.br>
2018-03-27 22:28:41 -0300

    API completa

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
-const app            = express();
+const db             = require('./config/db');
 
-const port = 8001;
+const app            = express();
+const port           = 8001;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require('./app/routes')(app, {});
-app.listen(port, () => {
-  console.log('We are live on ' + port);
-});
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+
+  //database already configured on db.js
+  require('./app/routes')(app, database);
+
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });
+})
